b676ad2e5087d6bdae9963472cc8f5bf88d9728a
Antoine Auger <Ardyn142@gmail.com>
2024-09-16 09:55:37 -0400

    Add Fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,9 @@
         <title inertia>{{ config('app.name', 'La faim des temps') }}</title>
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Montaga&display=swap" rel="stylesheet"> <!-- Font de titre-->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"> <!-- Font de base-->
+        <link href="https://fonts.googleapis.com/css2?family=Imperial+Script&display=swap" rel="stylesheet"> <!-- Font du menu -->
 
         <!-- Scripts -->
         @routes
